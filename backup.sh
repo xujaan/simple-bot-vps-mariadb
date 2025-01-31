be092c495345dcb7ebb61831f319672d1a31ece2
xujaan <ahmadakhussyuj.an@gmail.com>
2025-02-01 01:04:42 +0700

    Update backup.sh to use DB_HOST for database connection in mysqldump

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 BACKUP_FILE="/app/backups/backup_${TIMESTAMP}.sql"
 
 # Backup database
-mysqldump -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_FILE
+mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_FILE
 
 # Kirim file backup ke Telegram menggunakan script Python
 python3 /app/send_to_telegram.py $BACKUP_FILE
