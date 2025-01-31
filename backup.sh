ea0a98ae21d036b3edab79e069dc609e6319645b
xujaan <ahmadakhussyuj.an@gmail.com>
2025-02-01 01:43:02 +0700

    Update backup script to include database name in backup file naming convention

--- backup.sh
+++ backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Buat nama file backup dengan timestamp
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-BACKUP_FILE="/app/backups/backup_${TIMESTAMP}.sql"
+BACKUP_FILE="/app/backups/backup_${DB_NAME}_${TIMESTAMP}.sql"
 
 # Backup database
 mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_FILE
 
-BACKUP_FILE2="/app/backups/backup_${TIMESTAMP}_db2.sql"
+BACKUP_FILE2="/app/backups/backup_${DB_NAME2}_${TIMESTAMP}.sql"
 mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME2 > $BACKUP_FILE2
 
 # Kirim file backup ke Telegram menggunakan script Python
