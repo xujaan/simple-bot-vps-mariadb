e87922536776f72729da5467bebecdb4a595c82a
xujaan <ahmadakhussyuj.an@gmail.com>
2025-02-01 00:27:30 +0700

    Add DB_HOST variable to .env.example and update backup script for database host configuration

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Backup database
-mysqldump -u $DB_USER -p$DB_PASSWORD $DB_NAME > /app/backups/backup.sql
+mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME > /app/backups/backup.sql
 
 # Kirim file backup ke Telegram menggunakan script Python
 python3 /app/send_to_telegram.py
